f38f7ebef74c7bd04431d465290d49b2e8722bc0
wangmingqiang <244905591@qq.com>
2017-03-15 18:31:45 +0800

    圆形图像的处理

--- app/src/main/java/com/wangmingqiang/money/MainActivity.java
+++ app/src/main/java/com/wangmingqiang/money/MainActivity.java
@@ -1,21 +1,22 @@
 package com.wangmingqiang.money;
 
-import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.wangmingqiang.money.activity.BaseActivity;
 import com.wangmingqiang.money.fragment.HomeFragment;
 import com.wangmingqiang.money.fragment.InvestFragment;
 import com.wangmingqiang.money.fragment.MoreFragment;
 import com.wangmingqiang.money.fragment.PropertyFragment;
+import com.wangmingqiang.money.utils.AppManager;
 
-import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @InjectView(R.id.main_fl)
     FrameLayout mainFl;
@@ -26,81 +27,94 @@ public class MainActivity extends AppCompatActivity {
     private MoreFragment moreFragment;
     private PropertyFragment propertyFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
+    public void initListener() {
 
-        switchFragment(R.id.rb_main);
-        initlistener();
-        
-    }
-
-
-
-    private void initlistener() {
         mainRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+
                 switchFragment(checkedId);
             }
         });
-        mainRg.check(R.id.rb_main);
     }
 
     private void switchFragment(int checkedId) {
-        //开启事务
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        hiddenFragment(ft);
-        switch (checkedId){
-            case R.id.rb_main:
-                if(homeFragment==null) {
-                    homeFragment = new HomeFragment();
-                    ft.add(R.id.main_fl,homeFragment);
-                }
-                ft.show(homeFragment);
-                break;
+        FragmentTransaction transaction =
+                getSupportFragmentManager().beginTransaction();
+
+        hiddenFragment(transaction);
+        switch (checkedId) {
             case R.id.rb_invest:
-                if(investFragment==null) {
+                if (investFragment == null) {
                     investFragment = new InvestFragment();
-                    ft.add(R.id.main_fl,investFragment);
+                    transaction.add(R.id.main_fl, investFragment);
                 }
-                ft.show(investFragment);
+                transaction.show(investFragment);
+                break;
+            case R.id.rb_main:
+                if (homeFragment == null) {
+                    homeFragment = new HomeFragment();
+                    transaction.add(R.id.main_fl, homeFragment);
+                }
+                transaction.show(homeFragment);
                 break;
             case R.id.rb_more:
-                if(moreFragment==null) {
+                if (moreFragment == null) {
                     moreFragment = new MoreFragment();
-                    ft.add(R.id.main_fl,moreFragment);
+                    transaction.add(R.id.main_fl, moreFragment);
                 }
-                ft.show(moreFragment);
+                transaction.show(moreFragment);
                 break;
             case R.id.rb_propert:
-                if(propertyFragment==null) {
+                if (propertyFragment == null) {
                     propertyFragment = new PropertyFragment();
-                    ft.add(R.id.main_fl,propertyFragment);
+                    transaction.add(R.id.main_fl, propertyFragment);
                 }
-                ft.show(propertyFragment);
+                transaction.show(propertyFragment);
                 break;
         }
-        //提交事务
-        ft.commit();
+        transaction.commit();
     }
 
-    private void hiddenFragment(FragmentTransaction ft) {
-        if(homeFragment!=null) {
-            ft.hide(homeFragment);
+    //隐藏所有的fragment
+    private void hiddenFragment(FragmentTransaction transaction) {
+
+        if (homeFragment != null) {
+            transaction.hide(homeFragment);
         }
-        if(propertyFragment!=null) {
-            ft.hide(propertyFragment);
+        if (investFragment != null) {
+            transaction.hide(investFragment);
         }
-        if(moreFragment!=null) {
-            ft.hide(moreFragment);
+        if (moreFragment != null) {
+            transaction.hide(moreFragment);
         }
-        if(investFragment!=null) {
-            ft.hide(investFragment);
+        if (propertyFragment != null) {
+            transaction.hide(propertyFragment);
         }
     }
+
+    public void initData() {
+        //添加到APPManager
+        AppManager.getInstance().addActivity(this);
+        //选择默认的fragment
+        switchFragment(R.id.rb_main);
+    }
+
+    @Override
+    protected void initTitle() {
+
+    }
+
+    @Override
+    public int getLayoutid() {
+        // 去掉窗口标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏顶部的状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        return R.layout.activity_main;
+    }
+
+
 
 
     //双击退出
--- app/src/main/java/com/wangmingqiang/money/fragment/PropertyFragment.java
+++ app/src/main/java/com/wangmingqiang/money/fragment/PropertyFragment.java
@@ -1,6 +1,19 @@
 package com.wangmingqiang.money.fragment;
 
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+import com.wangmingqiang.money.MainActivity;
 import com.wangmingqiang.money.R;
+import com.wangmingqiang.money.bean.UserInfo;
+import com.wangmingqiang.money.command.AppNetConfig;
+import com.wangmingqiang.money.utils.BitmapUtils;
+
+import butterknife.InjectView;
 
 /**
  * Created by wangmingqiang on 2017/3/10.
@@ -8,6 +21,27 @@ import com.wangmingqiang.money.R;
 
 public class PropertyFragment extends BaseFragment {
 
+
+    @InjectView(R.id.tv_settings)
+    TextView tvSettings;
+    @InjectView(R.id.iv_me_icon)
+    ImageView ivMeIcon;
+    @InjectView(R.id.rl_me_icon)
+    RelativeLayout rlMeIcon;
+    @InjectView(R.id.tv_me_name)
+    TextView tvMeName;
+    @InjectView(R.id.rl_me)
+    RelativeLayout rlMe;
+    @InjectView(R.id.recharge)
+    ImageView recharge;
+    @InjectView(R.id.withdraw)
+    ImageView withdraw;
+    @InjectView(R.id.ll_touzi)
+    TextView llTouzi;
+    @InjectView(R.id.ll_touzi_zhiguan)
+    TextView llTouziZhiguan;
+    @InjectView(R.id.ll_zichan)
+    TextView llZichan;
 
     @Override
     protected int getLayoutid() {
@@ -22,10 +56,38 @@ public class PropertyFragment extends BaseFragment {
     @Override
     protected void initData(String json) {
 
+        MainActivity activity= (MainActivity) getActivity();
+        UserInfo user = activity.getUser();
+
+        //设置用户的名字
+        tvMeName.setText(user.getData().getName());
+
+        //设置头像
+        Picasso.with(getActivity())
+                .load(AppNetConfig.BASE_URL+"/images/tx.png")
+                .transform(new Transformation() {
+            @Override
+            public Bitmap transform(Bitmap bitmap) {
+
+                Bitmap circleBitmap = BitmapUtils.circleBitmap(bitmap);
+
+                bitmap.recycle(); //必须把原来的释放掉
+                return circleBitmap;
+            }
+
+            @Override
+            public String key() {
+                return ""; //不能为空否则会报错
+            }
+        }).into(ivMeIcon);
     }
 
     @Override
     public String getChildUrl() {
         return null;
     }
+
+
+
+
 }
